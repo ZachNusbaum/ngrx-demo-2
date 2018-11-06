f68016f6cbcd34f9b1adad915e7c8e659fc5f5fd
Zach Nusbaum <zach@zdnmail.com>
2018-11-05 20:03:45 -0800

    Basic example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+//import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
+
+interface AppState {
+  message: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgrxDemo';
+  message$: Observable<string>;
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message');
+  }
+
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'});
+  }
+
+  frenchMessage() {
+    this.store.dispatch({type: 'FRENCH'});
+  }
 }
